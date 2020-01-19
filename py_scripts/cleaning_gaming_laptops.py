93d187ef6798ec8006bd3e4f5864008ce49414a3
joeyhuaa <joeyhua17@gmail.com>
2020-01-19 04:26:14 -0800

    joey worked on clean_gaming_laptops

--- py_scripts/cleaning_gaming_laptops.py
+++ py_scripts/cleaning_gaming_laptops.py
@@ -1,59 +1,41 @@
-#!/usr/bin/env python
-# coding: utf-8
 import pandas as pd
 import numpy as np
 import re
-# In[267]:
+
 pd.set_option('display.width', 500)
 pd.set_option('display.max_columns', 15)
 
 # Storing Gaming Laptops info
 gaming_laptops_df = pd.read_csv("../data/gaming_laptops_raw.csv", sep=",")
-gaming_laptops_df.head()
-
-# In[268]:
-
-
-gaming_laptops_df.cpu_speed.unique()
-
-# Replacing the NaN Values that are in Gaming Laptops info
-gaming_laptop_title = gaming_laptops_df.loc[0, "name"]
-gaming_laptop_title.split(" ")
 gaming_laptops_df = gaming_laptops_df.fillna(-1)  # Will get back to later
 
 # Converting Rating Values into Integers
-def clean_ratings(df):
-    for i in range(len(df["rating"])):
-        df["rating"] = int(df.loc[i, "rating"])
-    return df
-
-gaming_laptops_df = gaming_laptops_df.fillna(-1)
+def clean_ratings():
+    for i in range(len(gaming_laptops_df["rating"])):
+        gaming_laptops_df["rating"] = int(gaming_laptops_df.loc[i, "rating"])
 
 # Converting Screen Size Values into Floats
-def clean_screen_sizes(df):
+def clean_screen_sizes():
     # turn all screen sizes into floats
-    for i in range(len(df['screen'])):
-        if df.loc[i, 'screen'] == 'No':
-            df.loc[i, 'screen'] = -1
-        if type(df.loc[i, 'screen']) == str:
-            df.loc[i, 'screen'] = float(df.loc[i, 'screen'].split(' ')[0].replace('"', ''))
+    for i in range(len(gaming_laptops_df['screen'])):
+        if gaming_laptops_df.loc[i, 'screen'] == 'No':
+            gaming_laptops_df.loc[i, 'screen'] = -1
+        if type(gaming_laptops_df.loc[i, 'screen']) == str:
+            gaming_laptops_df.loc[i, 'screen'] = float(gaming_laptops_df.loc[i, 'screen'].split(' ')[0].replace('"', ''))
 
-    return df
+def clean_cores():
+    for i in range(len(gaming_laptops_df['cores'])):
+        if gaming_laptops_df.loc[i, 'cores'] == -1:
+            gaming_laptops_df.loc[i, 'cores'] = 6
+        elif 'Dual' in gaming_laptops_df.loc[i, 'cores']:
+            gaming_laptops_df.loc[i, 'cores'] = 2
+        elif 'Quad' in gaming_laptops_df.loc[i, 'cores'] or '4' in gaming_laptops_df.loc[i, 'cores']:
+            gaming_laptops_df.loc[i, 'cores'] = 4
+        elif 'Six' in gaming_laptops_df.loc[i, 'cores'] or '6' in gaming_laptops_df.loc[i, 'cores']:
+            gaming_laptops_df.loc[i, 'cores'] = 6
+        elif "8" in gaming_laptops_df.loc[i, "cores"]:
+            gaming_laptops_df.loc[i, "cores"] = 8
 
-def clean_cores(df):
-    for i in range(len(df['cores'])):
-        if df.loc[i, 'cores'] == -1:
-            pass
-        elif 'Dual' in df.loc[i, 'cores']:
-            df.loc[i, 'cores'] = 2
-        elif 'Quad' in df.loc[i, 'cores']:
-            df.loc[i, 'cores'] = 4
-        elif '7' in df.loc[i, 'cores']:
-            df.loc[i, 'cores'] = 7
-        elif "9" in df.loc[i, "cores"]:
-            df.loc[i, "cores"] = 9
-
-    return df
 
 def clean_gpu_type():
     for i in range(len(gaming_laptops_df["gpu_type"])):
@@ -104,109 +86,61 @@ def clean_gpu_type():
             gaming_laptops_df.loc[i, "gpu_type"] = 2080
 
 
-for i in range(len(gaming_laptops_df["gpu_memory"])):
-    if "-1" in gaming_laptops_df["gpu_memory"]:
-        pass
-    elif "shared system memory" in gaming_laptops_df["gpu_memory"]:
-        gaming_laptops_df.loc[i, "gpu_memory"] = 1
-    elif "2" in gaming_laptops_df["gpu_memory"]:
-        gaming_laptops_df.loc[i, "gpu_memory"] = 2
-    elif "3" in gaming_laptops_df["gpu_memory"]:
-        gaming_laptops_df.loc[i, "gpu_memory"] = 3
-    elif "4" in gaming_laptops_df["gpu_memory"] or "Up to 4" in gaming_laptops_df["gpu_memory"] or "4 GB GDDR5" in \
-            gaming_laptops_df["gpu_memory"]:
-        gaming_laptops_df.loc[i, "gpu_memory"] = 4
-    elif "6" in gaming_laptops_df["gpu_memory"] or "6G GDDR6" in gaming_laptops_df["gpu_memory"]:
-        gaming_laptops_df.loc[i, "gpu_memory"] = 6
-    elif "16" in gaming_laptops_df["gpu_memory"]:
-        gaming_laptops_df.loc[i, "gpu_memory"] = 16
-
-# In[279]:
-
-
-# Cleaning Gaming Laptop's General Memory
-gaming_laptops_df.memory.unique()
+def clean_gpu_memory():
+    for i in range(len(gaming_laptops_df["gpu_memory"])):
+        if "-1" in gaming_laptops_df["gpu_memory"]:
+            pass
+        elif "shared system memory" in gaming_laptops_df["gpu_memory"]:
+            gaming_laptops_df.loc[i, "gpu_memory"] = 1
+        elif "2" in gaming_laptops_df["gpu_memory"]:
+            gaming_laptops_df.loc[i, "gpu_memory"] = 2
+        elif "3" in gaming_laptops_df["gpu_memory"]:
+            gaming_laptops_df.loc[i, "gpu_memory"] = 3
+        elif "4" in gaming_laptops_df["gpu_memory"] or "Up to 4" in gaming_laptops_df["gpu_memory"] or "4 GB GDDR5" in \
+                gaming_laptops_df["gpu_memory"]:
+            gaming_laptops_df.loc[i, "gpu_memory"] = 4
+        elif "6" in gaming_laptops_df["gpu_memory"] or "6G GDDR6" in gaming_laptops_df["gpu_memory"]:
+            gaming_laptops_df.loc[i, "gpu_memory"] = 6
+        elif "16" in gaming_laptops_df["gpu_memory"]:
+            gaming_laptops_df.loc[i, "gpu_memory"] = 16
 
 
-# In[280]:
-gaming_laptops_df["memory"].fillna("16 GB", inplace = True)
-
-def clean_memory(df):
+def clean_memory():
     # retrieve GB ints from raw data
-    for i in range(len(df['memory'])):
-        if type(df.loc[i, 'memory']) == int or type(df.loc[i, 'memory']) == 'numpy.64int':
+    for i in range(len(gaming_laptops_df['memory'])):
+        if type(gaming_laptops_df.loc[i, 'memory']) == int or type(gaming_laptops_df.loc[i, 'memory']) == 'numpy.64int':
             continue
         else:
-            print(i, df.loc[i, 'memory'])
-            df.loc[i, 'memory'] = int(df.loc[i, 'memory'].split(' ')[0])
-
-    return df
+            gaming_laptops_df.loc[i, 'memory'] = int(gaming_laptops_df.loc[i, 'memory'].split(' ')[0])
 
 
-# In[281]:
-
-
-# In[282]:
-
-
-# Cleaning Laptop's CPU Speed
-gaming_laptops_df.cpu_speed.unique()
-
-
-# In[284]:
-
-
-def clean_cpu_speed(df):
-    for i in range(len(df['cpu_speed'])):
+def clean_cpu_speed():
+    for i in range(len(gaming_laptops_df['cpu_speed'])):
         # missing values
-        if df.loc[i, 'cpu_speed'] == -1:
+        if gaming_laptops_df.loc[i, 'cpu_speed'] == -1:
             pass
 
         # case with extra words at beginning
-        elif '(' in df.loc[i, 'cpu_speed']:
-            df.loc[i, 'cpu_speed'] = float(df.loc[i, 'cpu_speed'].split('(')[1].replace(')', '').split(' ')[0])
+        elif '(' in gaming_laptops_df.loc[i, 'cpu_speed']:
+            gaming_laptops_df.loc[i, 'cpu_speed'] = float(gaming_laptops_df.loc[i, 'cpu_speed'].split('(')[1].replace(')', '').split(' ')[0])
 
         # when GHz is at the end
-        elif 'GHz' in df.loc[i, 'cpu_speed']:
-            df.loc[i, 'cpu_speed'] = float(df.loc[i, 'cpu_speed'].split(' ')[0])
+        elif 'GHz' in gaming_laptops_df.loc[i, 'cpu_speed']:
+            gaming_laptops_df.loc[i, 'cpu_speed'] = float(gaming_laptops_df.loc[i, 'cpu_speed'].split(' ')[0])
 
-    return df
+def clean_prices():
+    for i in range(len(gaming_laptops_df['price'])):
+        if type(gaming_laptops_df.loc[i, 'price']) == str:
+            gaming_laptops_df.loc[i, 'price'] = float(gaming_laptops_df.loc[i, 'price'].replace(',', ''))
+        elif gaming_laptops_df.loc[i, 'price'] == -1:
+            gaming_laptops_df.loc[i, 'price'] = 1493.64  # fill missing with the mean price
 
-
-# In[285]:
-
-
-# Cleaning Gaming Laptops' Prices
-float(gaming_laptops_df.loc[14, 'cpu_speed'].split('(')[1].replace(')', '').split(' ')[0])
-
-
-# In[286]:
-
-
-def clean_prices(df):
-    for i in range(len(df['price'])):
-        if type(df.loc[i, 'price']) == str:
-            df.loc[i, 'price'] = float(df.loc[i, 'price'].replace(',', ''))
-
-    return df
-
-
-# In[287]:
-
-
-# Cleaning Gaming Laptops' Storage
-gaming_laptops_df.storage.unique()
-
-
-# In[288]:
-
-
-def clean_storage(df):
-    for i in range(len(df['storage'])):
-        if df.loc[i, 'storage'] != -1:
-            # df.loc[i, 'storage'] = re.split('[A-z]|,', df.loc[i, 'storage'])[0]
-            df.loc[i, 'storage'] = df.loc[i, 'storage'].replace('+', ',')
-            storage_split_list = re.split('[A-z]|,', df.loc[i, 'storage'])
+def clean_storage():
+    for i in range(len(gaming_laptops_df['storage'])):
+        if gaming_laptops_df.loc[i, 'storage'] != -1:
+            # gaming_laptops_df.loc[i, 'storage'] = re.split('[A-z]|,', gaming_laptops_df.loc[i, 'storage'])[0]
+            gaming_laptops_df.loc[i, 'storage'] = gaming_laptops_df.loc[i, 'storage'].replace('+', ',')
+            storage_split_list = re.split('[A-z]|,', gaming_laptops_df.loc[i, 'storage'])
 
             # retrieve integer values from raw data
             storage_int = 0
@@ -223,25 +157,17 @@ def clean_storage(df):
                     pass
 
             # assign new value to storage column
-            df.loc[i, 'storage'] = storage_int
+            gaming_laptops_df.loc[i, 'storage'] = storage_int
 
-    return df
-
-
-# In[289]:
-
-
-# Write to astudy_df = clean_prices(study_df)
-gaming_laptops_df = clean_cpu_speed(gaming_laptops_df)
-gaming_laptops_df = clean_cores(gaming_laptops_df)
-gaming_laptops_df = clean_storage(gaming_laptops_df)
-gaming_laptops_df = clean_memory(gaming_laptops_df)
-gaming_laptops_df = clean_screen_sizes(gaming_laptops_df)
+## MAIN
+clean_prices()
+clean_ratings()
+clean_cpu_speed()
+clean_cores()
+clean_storage()
+clean_memory()
+clean_gpu_memory()
+clean_gpu_type()
+clean_screen_sizes()
 
 gaming_laptops_df.to_csv("gaming_laptop_cleaned.csv")
-
-# In[219]:
-
-
-gaming_laptops_df.cpu_speed.unique()
-
--- py_scripts/cleaning_study_laptops.py
+++ py_scripts/cleaning_study_laptops.py
@@ -7,58 +7,57 @@ pd.set_option('display.max_columns', 15)
 
 # csv into pd data frame
 study_df = pd.read_csv('study_laptops_raw.csv', sep=',')
-gaming_df = pd.read_csv('gaming_laptops_raw.csv', sep=',')
 
 # temporarily fill all N/A with -1
 study_df = study_df.fillna(-1)
 
 # turn all prices into floats
-def clean_prices(df):
-    for i in range(len(df['price'])):
-        if type(df.loc[i, 'price']) == str:
-            df.loc[i, 'price'] = float(df.loc[i, 'price'].replace(',', ''))
+def clean_prices(d):
+    for i in range(len(study_df['price'])):
+        if type(study_df.loc[i, 'price']) == str:
+            study_df.loc[i, 'price'] = float(study_df.loc[i, 'price'].replace(',', ''))
 
-    return df
+    return study_df
 
 # remove strings from cpu_speed, leave only GHz, turn into floats
-def clean_cpu_speed(df):
-    for i in range(len(df['cpu_speed'])):
+def clean_cpu_speed(study_df):
+    for i in range(len(study_df['cpu_speed'])):
         # missing values
-        if df.loc[i, 'cpu_speed'] == -1:
+        if study_df.loc[i, 'cpu_speed'] == -1:
             pass
 
         # case with extra words at beginning
-        elif '(' in df.loc[i, 'cpu_speed']:
-            df.loc[i, 'cpu_speed'] = float(df.loc[i,'cpu_speed'].split('(')[1].replace(')', '').split(' ')[0])
+        elif '(' in study_df.loc[i, 'cpu_speed']:
+            study_df.loc[i, 'cpu_speed'] = float(study_df.loc[i,'cpu_speed'].split('(')[1].replace(')', '').split(' ')[0])
 
         # when GHz is at the end
-        elif df.loc[i, 'cpu_speed'].split(' ')[1] == 'GHz':
-            df.loc[i, 'cpu_speed'] = float(df.loc[i, 'cpu_speed'].split(' ')[0])
+        elif study_df.loc[i, 'cpu_speed'].split(' ')[1] == 'GHz':
+            study_df.loc[i, 'cpu_speed'] = float(study_df.loc[i, 'cpu_speed'].split(' ')[0])
 
-    return df
+    return study_df
 
 # convert cores into integer values
-def clean_cores(df):
-    for i in range(len(df['cores'])):
-        if df.loc[i, 'cores'] == -1:
+def clean_cores(study_df):
+    for i in range(len(study_df['cores'])):
+        if study_df.loc[i, 'cores'] == -1:
             pass
-        elif 'Dual' in df.loc[i, 'cores']:
-            df.loc[i, 'cores'] = 2
-        elif 'Quad' in df.loc[i, 'cores']:
-            df.loc[i, 'cores'] = 4
-        elif '6' in df.loc[i, 'cores']:
-            df.loc[i, 'cores'] = 6
+        elif 'Dual' in study_df.loc[i, 'cores']:
+            study_df.loc[i, 'cores'] = 2
+        elif 'Quad' in study_df.loc[i, 'cores']:
+            study_df.loc[i, 'cores'] = 4
+        elif '6' in study_df.loc[i, 'cores']:
+            study_df.loc[i, 'cores'] = 6
 
-    return df
+    return study_df
 
 # leave only GB in storage, convert to int
 # create a new column called cpu_type that = SSD or HDD
-def clean_storage(df):
-    for i in range(len(df['storage'])):
-        if df.loc[i, 'storage'] != -1:
-            # df.loc[i, 'storage'] = re.split('[A-z]|,', df.loc[i, 'storage'])[0]
-            df.loc[i, 'storage'] = df.loc[i, 'storage'].replace('+', ',')
-            storage_split_list = re.split('[A-z]|,', df.loc[i, 'storage'])
+def clean_storage(study_df):
+    for i in range(len(study_df['storage'])):
+        if study_df.loc[i, 'storage'] != -1:
+            # study_df.loc[i, 'storage'] = re.split('[A-z]|,', study_df.loc[i, 'storage'])[0]
+            study_df.loc[i, 'storage'] = study_df.loc[i, 'storage'].replace('+', ',')
+            storage_split_list = re.split('[A-z]|,', study_df.loc[i, 'storage'])
 
             # retrieve integer values from raw data
             storage_int = 0
@@ -75,36 +74,36 @@ def clean_storage(df):
                     pass
 
             # assign new value to storage column
-            df.loc[i, 'storage'] = storage_int
+            study_df.loc[i, 'storage'] = storage_int
 
-    return df
+    return study_df
 
-def clean_memory(df):
+def clean_memory(study_df):
     # retrieve GB ints from raw data
-    for i in range(len(df['memory'])):
-        df.loc[i, 'memory'] = int(df.loc[i, 'memory'].split(' ')[0])
+    for i in range(len(study_df['memory'])):
+        study_df.loc[i, 'memory'] = int(study_df.loc[i, 'memory'].split(' ')[0])
 
-    return df
+    return study_df
 
 
-def clean_screen_sizes(df):
+def clean_screen_sizes(study_df):
     # turn all screen sizes into floats
-    for i in range(len(df['screen'])):
-        if df.loc[i, 'screen'] == 'No':
-            df.loc[i, 'screen'] = -1
-        if type(df.loc[i, 'screen']) == str:
-            df.loc[i, 'screen'] = float(df.loc[i, 'screen'].replace('"', ''))
+    for i in range(len(study_df['screen'])):
+        if study_df.loc[i, 'screen'] == 'No':
+            study_df.loc[i, 'screen'] = -1
+        if type(study_df.loc[i, 'screen']) == str:
+            study_df.loc[i, 'screen'] = float(study_df.loc[i, 'screen'].replace('"', ''))
 
-    return df
+    return study_df
 
 
 ## MAIN ##
-study_df = clean_prices(study_df)
-study_df = clean_cpu_speed(study_df)
-study_df = clean_cores(study_df)
-study_df = clean_storage(study_df)
-study_df = clean_memory(study_df)
-study_df = clean_screen_sizes(study_df)
+clean_prices(study_study_df)
+clean_cpu_speed(study_study_df)
+clean_cores(study_study_df)
+clean_storage(study_study_df)
+clean_memory(study_study_df)
+clean_screen_sizes(study_study_df)
 
 # write to csv
 
